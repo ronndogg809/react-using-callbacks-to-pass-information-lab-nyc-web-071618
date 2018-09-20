5b47a3d0dece5b016be7e3a2cf5465f865af0c6a
github username <github email address>
2018-09-20 09:13:52 -0400

    Done.

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
 export default class ColorSelector extends Component {
-  
+
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+      const callback = () => {this.props.setColor(str)}
+  return <div key={idx} className="color-swatch" style={{backgroundColor: str}} onClick={callback}/>
+
     })
   )
-  
+
   render() {
     return (
       <div id="colorSelector">
@@ -15,5 +17,5 @@ export default class ColorSelector extends Component {
       </div>
     )
   }
-  
+
 }
--- src/Matrix.js
+++ src/Matrix.js
@@ -7,10 +7,21 @@ export default class Matrix extends Component {
 
   constructor() {
     super()
+    this.state = {
+   color: '#fff'
+ }
+}
+setColor = (newColor) => {
+ this.setState({
+   color: newColor
+ })
+}
+getColor = () => {
+ return this.state.color
   }
 
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} getColor={this.getColor} />)
   )
 
   genMatrix = () => (
@@ -21,7 +32,7 @@ export default class Matrix extends Component {
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+      <ColorSelector setColor={this.setColor} />
         <div id="matrix">
           {this.genMatrix()}
         </div>
